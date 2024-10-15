f2834a9254d3bc662243ddf8cab6a3866935133d
ingefaerel <elmiramust2010@gmail.com>
2024-10-15 20:12:39 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+    for each web dev skill that you have,
+    customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="./linkdn.png" alt="Elmira Mustafina" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Elmira Mustafina</h1>
+      <p>
+        A junior frontend developer and a graphic designer. I like to play music
+        and sing for fun.{" "}
+      </p>
+    </div>
+  );
+}
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="JavaScript" emoji="😃" color="#e6acf1" number={1} />
+      <Skill skill="HTML" emoji="😝" color="lightBlue" number={2} />
+      <Skill skill="CSS" emoji="👂" color="#66ad66" number={3} />
+      <Skill skill="React" emoji="🤜" color="pink" number={4} />
+      <Skill skill="Jest" emoji="👀" color="#f7de72" number={5} />
+      <Skill skill="Git" emoji="💪" color="#88caff" number={6} />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.number}</span>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
     </div>
   );
 }
